2105011ae3691e69199862410876b9b9ec66a67d
asha cook <acook2@collegeforcreativestudies.edu>
2018-04-18 14:27:12 -0400

    ashas changes

---
+++ section-5 /scripts/app.js
@@ -0,0 +1,14 @@
+/*
+$('.btn1').fadeOut({
+
+}, 1000);
+*/
+
+
+$(document).ready(function() {
+
+$('.button01').click(function() {
+	$('.puke').fadeOut();
+});
+
+});
---
+++ section-6 /scripts/app.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+$("#phone").rotate({angle:25});
+
+$("#phone01").rotate({angle:45});
+
+$("#phone02").rotate({angle:65});
+
+$("#phone03").rotate({angle:85});
+
+$("#phone04").rotate({angle:105});
+
+});
