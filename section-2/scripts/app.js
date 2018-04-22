e17d81ee97b8d45d87ec22aba09d002ef34d01f8
emilyjohnson <ejohnson7@collegeforcreativestudies.edu>
2018-04-22 14:00:59 -0400

    emily sunday updates

--- section-2/scripts/app.js
+++ section-2/scripts/app.js
@@ -1,13 +1,43 @@
-$( document ).ready(function() {
-  $( ".person-1" ).effect( "shake", "slow");
-});
+// $( document ).ready(function() {
+//   $( ".person-1" ).effect( "shake", "slow");
+// }, 1000);
+//
+// $( document ).ready(function() {
+//   $( ".person-2" ).effect( "shake" );
+// });
+//
+// $( document ).ready(function() {
+//   $( ".person-3" ).effect( "pulsate", "slow" );
+// });
+//
+// $(document).setInterval(function() {
+//     $(".person-4").effect("bounce",2000)
+// }, 1000);
 
-$( document ).ready(function() {
-  $( ".person-2" ).effect( "easeOutBounce" );
-});
 
-$( document ).ready(function() {
-  $( ".person-3" ).effect( "pulsate", "slow" );
+$(document).ready(function(){
+
+      setInterval(function() {
+        $( ".person-3" ).effect( "pulsate", "slow" )
+        }, 1000);
+
+      setInterval(function() {
+        $('.person-4').effect('bounce',1000)
+        }, 1000);
+
+      setInterval(function() {
+        $('.person-1').effect('shake', "slow")
+        }, 2000);
+
+      setInterval(function() {
+        $('.person-2').effect('bounce')
+      }, 800);
+
+      setInterval(function() {
+        $( ".person-5" ).effect( "pulsate", 20)
+      }, 500);
+
+
 });
 
 
