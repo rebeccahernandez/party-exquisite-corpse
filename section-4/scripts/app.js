151665a5dd8223e863fec831223db18eb7755937
emilyjohnson <ejohnson7@collegeforcreativestudies.edu>
2018-04-23 14:57:45 -0400

    class shit

--- section-4/scripts/app.js
+++ section-4/scripts/app.js
@@ -3,21 +3,21 @@ $(document).ready(function() {
   $('.ball').draggable({
   });
 
-//bounce when click anywhere
-$(document).ready(function() {
-    $( ".ball" ).effect("bounce", { times:4, distance:200 }, 400);
-  });
+ //bounce when click anywhere
+$(document).click(function() {
+     $( ".ball" ).effect("bounce", { times:4, distance:200 }, 400);
+   });
 
 
 });
 
-
-$(document).ready(function(){
-
-      setInterval(function() {
-        $( ".ball" ).effect( "bounce")
-        }, 1000);
-});
+//
+// $(document).ready(function(){
+//
+//       setInterval(function() {
+//         $( ".ball" ).effect( "bounce")
+//         }, 1000);
+// });
 
 
 //cursor
@@ -25,6 +25,14 @@ $(document).bind('mousemove', function(e){
   $('.ball').css({
     top: e.pageY - $(".ball").height()/2, // just minus by half the height
     left:  e.pageX - $(".ball").width()/2 // just minus by half the width
+  });
+
+});
+
+//droppable
+$(document).ready(function(){
+  $( ".cup" ).droppable({
+    accept: ".ball"
   });
 
 });
