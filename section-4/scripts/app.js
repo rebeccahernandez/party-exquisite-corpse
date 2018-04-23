e872b89587e51d042661fbbc72b4ded47b2a78d3
emilyjohnson <ejohnson7@collegeforcreativestudies.edu>
2018-04-23 14:31:23 -0400

    fetching!

--- section-4/scripts/app.js
+++ section-4/scripts/app.js
@@ -19,35 +19,12 @@ $(document).ready(function(){
         }, 1000);
 });
 
-//ball bouncing across screen
-$(document).ready(function(){
-   jQuery.fn.makeItBounce = function(){
 
-     //Let's do some style
-     this.width(1400).height(600).css('background-color', '#000');
-
-     for(var i=0; i<140;i++){
-       //Let's add something
-       this.append('<ball></ball>');
-       var ball = this.find('ball').eq(0);
-       ball.width(100).height(100).css('background-color', '#fff').css('display','inline-block').css('border-radius','50px').css('position','absolute').css('top',285);
-
-       var x,y,affX,affY;
-       var initX = parseInt(ball.css('left'));
-       var initY = parseInt(ball.css('top'));
-
-       var rdX = Math.floor(Math.random() * 50) + 15;
-       var rdY = Math.floor(Math.random() * 100) + 200;
-
-       for (x = 1 ; x < 28; x = x+.2){
-         y = (Math.abs(Math.sin(x)))/x;
-         affX = initX + x * rdX;
-         affY = initY - y * rdY;
-         ball.animate({left: affX, top: affY},10);
-       }
-     }
-   }
-
-   $('div.area').makeItBounce();
+//cursor
+$(document).bind('mousemove', function(e){
+  $('.ball').css({
+    top: e.pageY - $(".ball").height()/2, // just minus by half the height
+    left:  e.pageX - $(".ball").width()/2 // just minus by half the width
+  });
 
 });
