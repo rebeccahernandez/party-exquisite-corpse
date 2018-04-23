550a3980787ca81676bc0ef754b65fe633727f80
emilyjohnson <ejohnson7@collegeforcreativestudies.edu>
2018-04-23 12:53:14 -0400

    sec 3

--- section-3/scripts/app.js
+++ section-3/scripts/app.js
@@ -1,9 +1,8 @@
-$(document).ready(function() {
-
-  $(document).bind('mousemove', function(e){
-   $('.cursor').css({
-     top: e.pageY - $(".cursor").height()/2, // just minus by half the height
-     left:  e.pageX - $(".cursor").width()/2 // just minus by half the width
-   });
+//cursor
+$(document).bind('mousemove', function(e){
+  $('.cursor').css({
+    top: e.pageY - $(".cursor").height()/2, // just minus by half the height
+    left:  e.pageX - $(".cursor").width()/2 // just minus by half the width
+  });
 
 });
